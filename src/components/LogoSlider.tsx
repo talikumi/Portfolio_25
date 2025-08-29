704bf3cf529af2f03b09ff12949f7cbd637246df
Nina T <nina@MacBook-Air-di-Nina.local>
2025-08-29 18:08:30 +0200

    Fix all remaining FontAwesome import errors - use only verified icons from free brands package

--- src/components/LogoSlider.tsx
+++ src/components/LogoSlider.tsx
@@ -15,12 +15,12 @@ import {
   faWordpress,
   faShopify,
   faStripe,
-  faVercel,
   faGithub,
   faNpm,
   faLinkedin,
   faTwitter,
-  faInstagram
+  faInstagram,
+  faFacebook
 } from '@fortawesome/free-brands-svg-icons';
 import { gsap } from '@/lib/gsap';
 
@@ -42,12 +42,12 @@ const LogoSlider = () => {
     { icon: faWordpress, name: 'WordPress', color: '#21759B' },
     { icon: faShopify, name: 'Shopify', color: '#7AB55C' },
     { icon: faStripe, name: 'Stripe', color: '#008CDD' },
-    { icon: faVercel, name: 'Vercel', color: '#000000' },
     { icon: faGithub, name: 'GitHub', color: '#181717' },
     { icon: faNpm, name: 'npm', color: '#CB3837' },
     { icon: faLinkedin, name: 'LinkedIn', color: '#0A66C2' },
     { icon: faTwitter, name: 'Twitter', color: '#1DA1F2' },
-    { icon: faInstagram, name: 'Instagram', color: '#E4405F' }
+    { icon: faInstagram, name: 'Instagram', color: '#E4405F' },
+    { icon: faFacebook, name: 'Facebook', color: '#1877F2' }
   ];
 
   useEffect(() => {
